5a7a484dbc9c095db8771def951d909c3f125f2c
Yuhang(Steven) Wang <stevenaura@live.com>
2016-10-29 18:49:50 -0500

    move dependency modules to deps

--- src/build.bat
+++ src/build.bat
@@ -2,4 +2,4 @@ del amalg.cache
 lua -lamalg FunctionalX.lua
 lua amalg.lua -s FunctionalX.lua -c -o ..\release\FunctionalX.lua
 copy ..\release\FunctionalX.lua ..\..\luapower
-copy ..\release\FunctionalX.lua ..\..\umolflowFramework\src
+copy ..\release\FunctionalX.lua ..\..\umolflowFramework\src\deps
